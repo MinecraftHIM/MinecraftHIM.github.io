bf4799688ea4bb2b5301241c50277479e0200263
MinecraftHIM <102952048+MinecraftHIM@users.noreply.github.com>
2025-01-18 12:26:28 +0800

    1

--- assets/js/deng.js
+++
@@ -1,5 +0,0 @@
-const toggleSwitch = document.getElementById('toggleSwitch'); //  假设你有一个id为'toggleSwitch'的开关按钮
-
-toggleSwitch.addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
-});
